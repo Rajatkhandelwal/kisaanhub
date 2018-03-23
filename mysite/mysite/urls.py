6d7b230bc19f197af78c19c44eca8a760f012367
nktkarnany <ankitkarnany@gmail.com>
2018-03-23 14:20:38 +0530

    basic index template rendered

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^stats/', include('stats.urls')),
+    url(r'^', include('stats.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- mysite/stats/views.py
+++ mysite/stats/views.py
@@ -12,41 +12,12 @@ import os
 
 # Create your views here.
 def index(request):
-    temp_file = 'temp.txt'
-    data_type = 'min_temp'
-    country = 'uk'
-
-    r = requests.get(settings.DATA_ROOT_URL + settings.REGION_TYPE[country][data_type])
-    file = open(temp_file,'w')
-    file.write(r.text)
-    file.close()
-
-    with open(temp_file) as stats:
-        table = stats.readlines()[7:]
-
-    table_list = [row.split() for row in table]
-
-    table_dict = dict((z[0],list(z[1:])) for z in table_list[1:])
-
-    l = []
-
-    keys = [x.lower() for x in table_list[0][1:]]
-
-    for key, value in table_dict.iteritems():
-        update_fields = dict(zip(keys, [convert_to_float(i) for i in value]))
-        year = int(key)
-        if Data.objects.filter(data_type=data_type, country=country, year=year).exists():
-            l.append(update_fields)
-            Data.objects.filter(data_type=data_type, country=country, year=year).update(**update_fields)
-        else:
-            insert_fields = dict(data_type=data_type, country=country, year=year)
-            insert_fields.update(update_fields)
-            Data(**insert_fields).save()
-            l.append("not has")
-
-    os.remove(temp_file)
-
-    return HttpResponse(json.dumps(l), content_type="application/json")
+    try:
+        template = loader.get_template("index.html")
+        context = RequestContext(request, {})
+        return HttpResponse(template.render(context))
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(loader.get_template("404.html").render(RequestContext(request,{})))
 
 def convert_to_float(n):
     try:
@@ -61,7 +32,7 @@ def stats_data(request, country, data_type):
         if(data_type not in settings.REGION_TYPE[country]):
           return HttpResponseNotFound(loader.get_template("404.html").render(RequestContext(request,{})))
 
-        template = loader.get_template("index.html")
+        template = loader.get_template("stats.html")
 
         d = Data.objects.values().filter(data_type=data_type, country=country)
         data = json.dumps(list(d), cls=DjangoJSONEncoder)
